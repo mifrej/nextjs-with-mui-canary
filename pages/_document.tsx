d17b0a717ce986a7d7026ae13c85d9c96a724197
Milosz Frejnik <milosz@frejnik.com>
2019-05-29 15:34:50 +0200

    Switch to next canary with native TS support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,27 +32,34 @@ class MyDocument extends Document {
 }
 
 MyDocument.getInitialProps = async ctx => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
+
+  {
+    /**
+         * Resolution order
+
+        * On the server:
+        * 1. app.getInitialProps
+        * 2. page.getInitialProps
+        * 3. document.getInitialProps
+        * 4. app.render
+        * 5. page.render
+        * 6. document.render
+
+        * On the server with error:
+        * 1. document.getInitialProps
+        * 2. app.render
+        * 3. page.render
+        * 4. document.render
+
+        * On the client
+        * 1. app.getInitialProps
+        * 2. page.getInitialProps
+        * 3. app.render
+        * 4. page.render
+        *
+
+    */
+  }
 
   // Render app and page and get the context of the page with collected side effects.
   const sheets = new ServerStyleSheets();
